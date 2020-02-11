de4fa542232ed9b490f625d121ba83aa1420a7dd
Erin Boehlert <eringraceboehlert@gmail.com>
2020-02-10 22:00:55 -0700

    add endpoints to get rover manifest + photos

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,7 @@ const path = require('path')
 
 const app = express()
 const port = 3000
+const rover_endpoint = 'https://api.nasa.gov/mars-photos/api/v1';
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -13,6 +14,32 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+app.get('/rovers/:rover_name', async (req, res) => {
+    const { rover_name } = req.params;
+    try {
+        let rover = await fetch(`
+            ${rover_endpoint}/manifests/${req.params.rover_name}?api_key=${process.env.API_KEY}
+        `).then(res => res.json())
+
+        res.send(rover)
+    } catch (err) {
+        console.log('error: ', err)
+    }
+})
+
+app.get('/rover_photos/:rover_name/:max_date', async (req, res) => {
+    try {
+        const { rover_name, max_date } = req.params;
+        let rover_photos = await fetch(
+            `${rover_endpoint}/rovers/${rover_name}/photos?earth_date=${max_date}&api_key=${process.env.API_KEY}
+        `).then(res => res.json())
+
+        res.send(rover_photos)
+    } catch (err) {
+        console.log('error: ', err)
+    }
+})
+
 
 // example API call
 app.get('/apod', async (req, res) => {
